fca6d9fa98139a71ec0bfda8e33bc7239156bb42
rm46627 <michredk@gmail.com>
2023-11-19 21:36:20 +0100

    tworzenie i wyszukiwanie przedmiotow dla grupy

--- java/src/main/java/pl/ug/edu/fiszkord/groups/Group.java
+++ java/src/main/java/pl/ug/edu/fiszkord/groups/Group.java
@@ -1,5 +1,7 @@
 package pl.ug.edu.fiszkord.groups;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- java/src/main/java/pl/ug/edu/fiszkord/subjects/Subject.java
+++ java/src/main/java/pl/ug/edu/fiszkord/subjects/Subject.java
@@ -1,20 +1,30 @@
 package pl.ug.edu.fiszkord.subjects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import pl.ug.edu.fiszkord.groups.Group;
 
 @Data
-@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "_subject")
 public class Subject {
 
     @Id
     @GeneratedValue
     private Integer id;
 
+    @Column(unique = true)
+    private String name;
+
     @ManyToOne
-    @JoinColumn(name = "group_id")
+    @JsonBackReference
     private Group group;
+
+
 }
